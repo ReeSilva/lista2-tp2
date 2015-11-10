83282521ab5d4d74597bc7043898d3a51f8fa811
Renato Biancalana da Silva <renato.silva@justdigital.com.br>
2015-11-10 18:33:07 -0200

    First commit!

---
+++ ex1.c
@@ -0,0 +1,13 @@
+// PROGRAM SOMA_PARES
+// AUTOR: THIAGO KINJO
+// DATA: 10/11/2015
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  
+  // Declaração de variáveis
+  int iInicio, iFim, iResultado = 0;
+
+}
